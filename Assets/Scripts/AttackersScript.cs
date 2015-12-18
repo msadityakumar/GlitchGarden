b33ad59b1174287c0f97a99baa905f7c038bc4d8
Aditya kumar <aditya.m@yesgnome.com>
2015-12-18 19:00:15 +0530

    implemented object  animations

---
+++ Assets/Scripts/AttackersScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttackersScript : MonoBehaviour {
+
+
+	[Range(-1.5f,1.0f)] public float walkSpeed;
+	// Use this for initialization
+	void Start () {
+		Rigidbody2D myRigidBody = gameObject.AddComponent<Rigidbody2D> ();
+		myRigidBody.isKinematic = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.left * walkSpeed * Time.deltaTime);
+	}
+}
